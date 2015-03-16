0c77de3b972ed2db71f49cbe0f24e5acb6c42c38
Artemy Pestretsov <pestretsov@me.com>
2015-03-17 01:20:04 +0300

    Initial commit

---
+++ TSystems/HelloWorldJetty/src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorld extends HttpServlet
+{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+}
